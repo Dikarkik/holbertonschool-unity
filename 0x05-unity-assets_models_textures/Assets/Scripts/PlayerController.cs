8340cf15c3e83f3cd1ae977786a3bb93749e661e
KaroDev <karodev3@gmail.com>
2021-01-25 21:18:51 -0500

    Add 'PlayerController.cs' to handle AWSD movement and jump

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Handle user input so the player can move left, right, forward, backward, and diagonally using the WASD keys.
+/// The player jump when the Space button is pressed.
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+
+    // movement
+    private Vector3 moveDirection;
+    private float playerSpeed = 5f;
+
+    // jump
+    private Vector3 playerGravity;
+    private float gravityValue = -9.81f;
+    private float jumpHeight = 1.0f;
+
+	void Start() => controller = GetComponent<CharacterController>();
+
+	void Update()
+    {
+        // movement
+        moveDirection.x = Input.GetAxis("Horizontal");
+        moveDirection.z = Input.GetAxis("Vertical");
+
+        // jump
+        if(!controller.isGrounded)
+        {
+            playerGravity.y += gravityValue * Time.deltaTime;
+        }
+        else if(Input.GetButtonDown("Jump"))
+	    {
+            playerGravity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+		}
+
+        // movement (Unexpected behavior if place this line below 'jump' line)
+        controller.Move(moveDirection * Time.deltaTime * playerSpeed);
+		// jump
+		controller.Move(playerGravity * Time.deltaTime);
+    }
+}
