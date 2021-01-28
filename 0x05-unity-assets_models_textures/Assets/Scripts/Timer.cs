0a2afcbfd298886666af4906790dccc112d97a60
KaroDev <karodev3@gmail.com>
2021-01-28 13:14:02 -0500

    Add task 8: Clock's ticking

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Timer : MonoBehaviour
+{
+    public Text TimerText;
+    float timer;
+    float milliseconds;
+    float seconds;
+    float minutes;
+
+    void Start()
+    {
+        timer = 0;
+    }
+
+    void Update()
+    {
+        StopWatchCalcul();
+    }
+
+    void StopWatchCalcul()
+    {
+        timer += Time.deltaTime;
+        Debug.Log(timer);
+        minutes = (int)(timer / 60);
+        seconds = (int)(timer % 60);
+        milliseconds = (int)((timer - seconds) * 100) % 100;
+        TimerText.text = $"{minutes.ToString("0")}:{seconds.ToString("00")}:{milliseconds.ToString("00")}";
+    }
+
+
+}
