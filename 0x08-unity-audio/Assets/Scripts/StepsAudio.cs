e7ba753eee163e2b518741d7c2817b63fdc4ae40
KaroDev <karodev3@gmail.com>
2021-03-18 14:48:35 -0500

    [0x08. Unity - Audio] Task 4

--- 0x08-unity-audio/Assets/Scripts/StepsAudio.cs
+++ 0x08-unity-audio/Assets/Scripts/StepsAudio.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class StepsAudio : MonoBehaviour
 {
@@ -8,21 +8,45 @@ public class StepsAudio : MonoBehaviour
     public AudioSource playerAudioSource;
     public AudioClip footsteps_running_grass;
     public AudioClip footsteps_running_rock;
+    public AudioClip footsteps_landing_grass;
+    public AudioClip footsteps_landing_rock;
+    
+    public AudioMixer audioMixer;
+    private AudioMixerGroup runningMixerGroup;
+    private AudioMixerGroup landingMixerGroup;
+
+
+    private void Start()
+    {
+        runningMixerGroup = audioMixer.FindMatchingGroups("Running")[0];
+        landingMixerGroup = audioMixer.FindMatchingGroups("Landing")[0];
+    }
 
     private void Step()
     {
+        if (playerAudioSource.outputAudioMixerGroup.name != "Running")
+            playerAudioSource.outputAudioMixerGroup = runningMixerGroup;
+        
         if (isGrass)
             playerAudioSource.PlayOneShot(footsteps_running_grass);
         else
             playerAudioSource.PlayOneShot(footsteps_running_rock);
-
+    }
+    
+    private void Landing()
+    {
+        if (playerAudioSource.outputAudioMixerGroup.name != "Landing")
+            playerAudioSource.outputAudioMixerGroup = landingMixerGroup;
+        
+        if (isGrass)
+            playerAudioSource.PlayOneShot(footsteps_landing_grass);
+        else
+            playerAudioSource.PlayOneShot(footsteps_landing_rock);
     }
     
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        Debug.Log(hit.gameObject.name);
-
-        if (hit.gameObject.CompareTag("Grass")) //Change if we want incline
+        if (hit.gameObject.CompareTag("Grass"))
             isGrass = true;
         else
             isGrass = false;
