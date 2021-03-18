fc6e8901cdc5fe429b6974093c5210654f71d30b
KaroDev <karodev3@gmail.com>
2021-03-18 14:23:59 -0500

    [0x08. Unity - Audio] From task 0 to 3

---
+++ 0x08-unity-audio/Assets/Scripts/StepsAudio.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepsAudio : MonoBehaviour
+{
+    private bool isGrass = true;
+    public AudioSource playerAudioSource;
+    public AudioClip footsteps_running_grass;
+    public AudioClip footsteps_running_rock;
+
+    private void Step()
+    {
+        if (isGrass)
+            playerAudioSource.PlayOneShot(footsteps_running_grass);
+        else
+            playerAudioSource.PlayOneShot(footsteps_running_rock);
+
+    }
+    
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Debug.Log(hit.gameObject.name);
+
+        if (hit.gameObject.CompareTag("Grass")) //Change if we want incline
+            isGrass = true;
+        else
+            isGrass = false;
+    }
+}
