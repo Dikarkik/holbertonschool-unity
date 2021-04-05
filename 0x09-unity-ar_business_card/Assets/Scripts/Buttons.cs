9a3cb597310044acc0bf142978fcfca5d8a7cfc4
KaroDev <karodev3@gmail.com>
2021-04-05 16:47:36 -0500

    0x09. Unity - AR Business Card

---
+++ 0x09-unity-ar_business_card/Assets/Scripts/Buttons.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    public void LinkedIn()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/dianacarolinaquinterocaro/");
+    }
+    
+    public void Github()
+    {
+        Application.OpenURL("https://github.com/KaroDev3");
+    }
+    
+    public void Twitter()
+    {
+        Application.OpenURL("https://twitter.com/KaroDev3");
+    }
+}
