7e031c66d06e3e8515dedb505044d36a03dd719d
KaroDev <karodev3@gmail.com>
2021-04-06 20:12:41 -0500

    Animations in buttons and text using leantween

---
+++ 0x09-unity-ar_business_card/Assets/Scripts/Animations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+public class Animations : MonoBehaviour
+{
+    [SerializeField] private DefaultTrackableEventHandler targetEventHandler;
+    [SerializeField] private RectTransform linkedInButton;
+    [SerializeField] private RectTransform githubButton;
+    [SerializeField] private RectTransform twitterButton;
+    [SerializeField] private RectTransform text;
+    
+    private void Start()
+    {
+        SetElementsSize();
+    }
+
+    private void OnEnable()
+    {
+        targetEventHandler.onTargetDetected += RunAnimations;
+        targetEventHandler.onTargetLost += SetElementsSize;
+    }
+
+    private void OnDisable()
+    {
+        targetEventHandler.onTargetDetected -= RunAnimations;
+        targetEventHandler.onTargetLost -= SetElementsSize;
+    }
+
+    public void RunAnimations()
+    {
+        LeanTween.scale(linkedInButton, Vector3.one, 1f).setEase(LeanTweenType.easeOutBack);
+        LeanTween.scale(githubButton, Vector3.one, 1f).setEase(LeanTweenType.easeOutBack);
+        LeanTween.scale(twitterButton, Vector3.one, 1f).setEase(LeanTweenType.easeOutBack);
+        LeanTween.scale(text, Vector3.one, 1f).setEase(LeanTweenType.easeOutBack);
+    }
+
+    public void SetElementsSize()
+    {
+        linkedInButton.localScale = Vector3.zero;
+        githubButton.localScale = Vector3.zero;
+        twitterButton.localScale = Vector3.zero;
+        text.localScale = Vector3.zero;
+    }
+}
