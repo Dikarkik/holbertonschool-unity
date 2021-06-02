4d272a7c01fd34855a9354fee00c91590e3cddb5
KaroDev <karodev3@gmail.com>
2021-06-02 17:34:51 -0500

    Avoid plane selections immediately after pess "Cancel" button

--- 0x0C-unity-ar_slingshot_game/Assets/Scripts/ScriptExamples/TryRaycast.cs
+++ 0x0C-unity-ar_slingshot_game/Assets/Scripts/ScriptExamples/TryRaycast.cs
@@ -1,36 +1,32 @@
-﻿using System.Collections.Generic;
-using DataSystem;
+﻿using DataSystem;
 using EventNotifier;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
-using UnityEngine.XR.ARSubsystems;
 
 namespace GameSystem
 {
-    public class PlaneSelection : MonoBehaviour
+    // Doesn't work
+    public class TryRaycast : MonoBehaviour
     {
+        /*
         public Material normalPlane;
 
         public Material selectedPlane;
         
-        private bool _canClick = true; // Because UnityEngine.XR.ARFoundation.ARRaycastManager.Raycast doesn't handle LayerMask
-
         private ARPlane _selectedPlane;
     
         private MeshRenderer _meshRenderer;
         
-        private ARPlaneManager _planeManager;
+        public ARPlaneManager _planeManager;
         
-        private ARRaycastManager _raycastManager;
+        private RaycastHit _hit;
+
+        private Ray _ray;
+
+        public Camera _camera;
+
+        private readonly string _planeTag = "Plane";
         
-        private static List<ARRaycastHit> _hits = new List<ARRaycastHit>();
-
-        private void Awake()
-        {
-            _raycastManager = GetComponent<ARRaycastManager>();
-            _planeManager = GetComponent<ARPlaneManager>();
-        }
-
         private void OnEnable()
         {
             StartingEvents.OnConfirmPlaneSelection += OnConfirmPlaneSelection;
@@ -41,27 +37,35 @@ namespace GameSystem
         {
             StartingEvents.OnConfirmPlaneSelection -= OnConfirmPlaneSelection;
             StartingEvents.OnCancelPlaneSelection -= OnCancelPlaneSelection;
+
         }
 
         void Update()
         {
-            if (Input.touchCount == 0 || !_canClick)
+            if (Input.touchCount == 0)
                 return;
-            
-            if (_raycastManager.Raycast(Input.GetTouch(0).position, _hits, TrackableType.Planes))
+
+            _ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(_ray, out _hit))
             {
-                // Raycast hits are sorted by distance, so the first one
-                // will be the closest hit.
-                OnPlaneSelection((ARPlane)_hits[0].trackable);
+                if (_hit.transform.CompareTag(_planeTag))
+                {
+                    OnPlaneSelection(_hit.transform.gameObject);
+                }
             }
         }
 
-        private void OnPlaneSelection(ARPlane plane)
+        private void OnPlaneSelection(GameObject touchedPlane)
         {
-            _canClick = false;
             _planeManager.enabled = false;
+
+            foreach (var plane in _planeManager.trackables)
+            {
+                if (touchedPlane == plane.gameObject)
+                    _selectedPlane = plane;
+            }
             
-            _selectedPlane = plane;
             _meshRenderer = _selectedPlane.GetComponent<MeshRenderer>();
             _meshRenderer.material = selectedPlane;
             
@@ -85,11 +89,6 @@ namespace GameSystem
         {
             _meshRenderer.material = normalPlane;
             _planeManager.enabled = true;
-
-            // To avoid plane selections immediately after press "Cancel" button
-            Invoke(nameof(CanClick), 0.4f);
-        }
-
-        private void CanClick() => _canClick = true;
+        }*/
     }
 }
